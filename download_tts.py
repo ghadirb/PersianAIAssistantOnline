e44940fc7c488f066332e7514215acb33137f917
ghadirb <ghadirb@example.com>
2025-10-23 22:04:34 +0330

    Add: TTS download script + assets folder

---
+++ download_tts.py
@@ -0,0 +1,9 @@
+﻿import urllib.request
+import os
+
+url = 'https://github.com/rhasspy/piper/releases/download/2023.11.14-2/voice-fa-ir-gyro-medium.tar.gz'
+output = 'persian_tts.tar.gz'
+
+print('دانلود مدل فارسی...')
+urllib.request.urlretrieve(url, output)
+print('✅ تمام!')
