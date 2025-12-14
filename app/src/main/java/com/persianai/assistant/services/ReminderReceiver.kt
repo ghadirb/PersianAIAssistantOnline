c6bfe4685094d0f296a0f103474ff878c6fc77d7
Ghadir Baraty <ghadir.baraty@gmail.com>
2025-12-14 19:24:32 +0330

    اصلاح خطای بیلد

--- app/src/main/java/com/persianai/assistant/services/ReminderReceiver.kt
+++ app/src/main/java/com/persianai/assistant/services/ReminderReceiver.kt
@@ -1,6 +1,5 @@
 package com.persianai.assistant.services
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -9,10 +8,10 @@ import android.content.Intent
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.persianai.assistant.activities.AdvancedRemindersActivity
 import com.persianai.assistant.activities.FullScreenAlarmActivity
 import com.persianai.assistant.R
 import com.persianai.assistant.utils.SmartReminderManager
